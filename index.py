83d3c5f77888230824b39e05291a3779a4c75ab9
vijaya-bo <51104146+vijaya-bo@users.noreply.github.com>
2019-09-16 14:21:01 -0400

    Update index.py

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!Good Evening!\n'
+    return 'Hello World!Good Evening!!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
